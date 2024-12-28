51f7e21c4307a04d47bfbd7618029ff9b7813a98
btmaste <baybella24@gmail.com>
2024-12-27 21:30:23 -0500

    Moved numbers to HTML

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const numpad = document.querySelector("#numpad");
 
-for(let i = 0; i < 10; i++){
-    let digit = document.createElement("button");
-    digit.classList.toggle("digit");
-    digit.innerText = i;
-    numpad.appendChild(digit);
+first_num = 0
+second_num = 0
+
+function operate (num1, num2, operator){
+
 }
